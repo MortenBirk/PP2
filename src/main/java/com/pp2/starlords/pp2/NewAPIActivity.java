925a6955c44675f937249486b26dcd10a7b97fa5
MortenBirk <mortenhbirk@gmail.com>
2014-10-01 15:37:29 +0200

    Working NewApiActivity. Locations are updated at intervals and working. Only output as toast message

--- src/main/java/com/pp2/starlords/pp2/NewAPIActivity.java
+++ src/main/java/com/pp2/starlords/pp2/NewAPIActivity.java
@@ -1,16 +1,96 @@
 package com.pp2.starlords.pp2;
 
 import android.app.Activity;
+import android.location.Location;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import 	android.widget.Toast;
+import com.google.android.gms.common.GooglePlayServicesClient.ConnectionCallbacks;
+import com.google.android.gms.common.GooglePlayServicesClient.OnConnectionFailedListener;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 
+public class NewAPIActivity extends Activity implements
+        ConnectionCallbacks,
+        OnConnectionFailedListener,
+        LocationListener {
 
-public class NewAPIActivity extends Activity {
+    private LocationClient mLocationClient;
+
+    // Update frequency in milliseconds
+    private static final long UPDATE_INTERVAL = 5000;
+    // A fast frequency ceiling in milliseconds
+    private static final long FASTEST_INTERVAL = 1000;
+    // Define an object that holds accuracy and frequency parameters
+    LocationRequest mLocationRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_api);
+        mLocationClient = new LocationClient(this, this, this);
+        createLocationRequest();
+
+    }
+
+    private void createLocationRequest() {
+        // Create the LocationRequest object
+        mLocationRequest = LocationRequest.create();
+        // Use high accuracy
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY); //Allow this to change
+        //Set the update interval
+        mLocationRequest.setInterval(UPDATE_INTERVAL);
+        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
+    }
+
+    /*
+     * Called when the Activity becomes visible.
+     */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Connect the client.
+        mLocationClient.connect();
+    }
+
+    /*
+    * Called when the Activity is no longer visible.
+    */
+    @Override
+    protected void onStop() {
+        // Disconnecting the client invalidates it.
+        mLocationClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(Bundle dataBundle) {
+        // Display the connection status
+        Toast.makeText(this, "Connected Loc Client", Toast.LENGTH_SHORT).show();
+        mLocationClient.requestLocationUpdates(mLocationRequest, this);
+    }
+
+    @Override
+    public void onDisconnected() {
+        // Display the connection status
+        Toast.makeText(this, "Disconnected Loc Client",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        // Display the connection status
+        Toast.makeText(this, "Failed conn to Loc Cli",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        // Report to the UI that the location was updated
+        String msg = "Updated Location: " +
+                Double.toString(location.getLatitude()) + "," +
+                Double.toString(location.getLongitude());
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 }
