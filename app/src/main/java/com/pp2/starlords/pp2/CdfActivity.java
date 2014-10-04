9261f5795d283e072a4fc2beac639cba6e957f1d
Mads Jensen <madsjensen01@gmail.com>
2014-10-04 23:04:49 +0200

    Added graphs for both apis. Refactored

--- app/src/main/java/com/pp2/starlords/pp2/CdfActivity.java
+++ app/src/main/java/com/pp2/starlords/pp2/CdfActivity.java
@@ -25,10 +25,18 @@ public class CdfActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cdf);
+        
+        renderOldGraph();
+        renderNewGraph();
+        
+    }
 
+    private void renderOldGraph() {
 
-
-        LinearLayout graphLayout = (LinearLayout) findViewById(R.id.cdfview_graph);
+        /**
+            NEW API
+        **/
+        LinearLayout graphLayout = (LinearLayout) findViewById(R.id.cdfnewview_graph);
 
         //TimeEventData[] values = TimeEventsDataStore.getListAsTimeEventData();
 
@@ -81,7 +89,64 @@ public class CdfActivity extends Activity {
         graphView.addSeries(series);
         graphLayout.addView(graphView);
         
-
     }
 
+
+    private void renderOldGraph() {
+        /**
+            OLD API
+        **/
+        LinearLayout graphLayout = (LinearLayout) findViewById(R.id.cdfoldview_graph);
+
+        //TimeEventData[] values = TimeEventsDataStore.getListAsTimeEventData();
+
+        // TODO: replace empty array with ground truths
+        PP2ReadingData[] values = CDFHelper.graphData(FileLogger.parseFile("readingsOldAPI.log"), new ArrayList<Position>() );
+
+        GraphViewSeriesStyle style = new GraphViewSeriesStyle(
+                Color.parseColor("#FF8800"), 1);
+
+        GraphViewSeries series = new GraphViewSeries("CDF", style, values);
+
+        BarGraphView graphView = new BarGraphView(context, "");
+
+        graphView.getGraphViewStyle().setHorizontalLabelsColor(Color.GRAY);
+        graphView.getGraphViewStyle().setVerticalLabelsColor(Color.GRAY);
+        graphView.getGraphViewStyle().setGridColor(Color.TRANSPARENT);
+
+    //  graphView.setDrawDataPoints(true);
+    //  graphView.setDataPointsRadius(15f);
+        
+        graphView.getGraphViewStyle().setNumHorizontalLabels(values.length);
+        graphView.setManualYAxis(true);
+        
+        
+        double max = 0;
+        
+        for (int i = 0; i < values.length; i++) {
+            if( values[i].getY() > max) 
+                max = values[i].getY();
+        }
+        
+        graphView.setManualYAxisBounds(max, 0);
+
+        graphView.setCustomLabelFormatter(new CustomLabelFormatter() {
+            @Override
+            public String formatLabel(double value, boolean isValueX) {
+                if (isValueX) {
+                    //return "" + ((int) value);
+                    if (value == Math.floor(value))
+                        return ((int)value) + "";
+                } else {
+                    double s = value/1000;
+                    return "" + ( (double) Math.round((s * 100)))/ 100; // convert to s, leave two decimal points
+                }
+                
+                return null;
+            }
+        });
+
+        graphView.addSeries(series);
+        graphLayout.addView(graphView);
+    }
 }
